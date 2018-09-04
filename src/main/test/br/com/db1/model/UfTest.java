9beceb862ceb0e61437ad8690275310c294faf02
marcelo <marceloasf1999@gmail.com>
2018-09-04 17:23:10 -0300

    Projeto atualizado.

--- src/main/test/br/com/db1/model/UfTest.java
+++ src/main/test/br/com/db1/model/UfTest.java
@@ -1,26 +1,52 @@
+
 package br.com.db1.model;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UfTest {
-
+	private EntityManagerFactory factory;
+	private EntityManager manager;
+	
+	@Before
+	public void init(){
+		factory = Persistence.createEntityManagerFactory("db1start");
+		manager = factory.createEntityManager();
+	}
+	
+	@After
+	public void fim(){
+		factory.close();
+	}
+	
 	@Test
 	public void inserirTest() {
-		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory("db1start");
-		EntityManager manager = factory.createEntityManager();
-
 		Uf uf = new Uf();
-		uf.setNome("SP");
-
+		uf.setNome("RR");
+		
 		manager.getTransaction().begin();
 		manager.persist(uf);
 		manager.getTransaction().commit();
-
-		factory.close();
+	}
+	
+	@Test
+	public void updateTest(){
+		Query q = manager.createQuery("from uf where nome = :pNome");
+		q.setParameter("pNome", "RR");
+		q.setMaxResults(1);
+		Uf uf = (Uf) q.getSingleResult();
+				
+		System.out.println(uf);
+		
+		uf.setNome("AA");
+		manager.getTransaction().begin();
+		manager.persist(uf);
+		manager.getTransaction().commit();
 	}
 }
